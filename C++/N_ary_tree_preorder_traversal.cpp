10d6bded0a45bc0819fc7fad1c33565048e92768
eric82714 <0958820714ab@gmail.com>
2020-06-16 08:27:23 +0800

    Update N_ary_tree_preorder_traversal.cpp

--- C++/N_ary_tree_preorder_traversal.cpp
+++ C++/N_ary_tree_preorder_traversal.cpp
@@ -20,17 +20,17 @@ public:
 
 class Solution {
 public:
-    vector<int> postorder(Node* root) {
+    vector<int> preorder(Node* root) {
         vector<int> result;
         if(!root) return result;
         
+        result.push_back(root->val);
         if(!root->children.empty()) {
             for(auto child : root->children) {
-                vector<int> tmp = postorder(child);
+                vector<int> tmp = preorder(child);
                 for(auto n : tmp) result.push_back(n);
             }
         }
-        result.push_back(root->val);
         
         return result;
     }
